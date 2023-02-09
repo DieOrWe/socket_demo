fc615f532f9b3cfff90806ffc6278d6b54b72612
hbin007 <rlawnsxo10@naver.com>
2023-02-09 10:20:24 +0900

    mvcDTO

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,62 +1,77 @@
-import React, { useState } from 'react';
-import SockJS from 'sockjs-client';
-import Stomp from 'stompjs';
+import React, { useState } from "react";
+import SockJS from "sockjs-client";
+import Stomp from "stompjs";
 
 function App() {
-  const [stompClient, setStompClient] = useState(null);
-  const [connected, setConnected] = useState(false);
-  const [greetings, setGreetings] = useState([]);
+    const [stompClient, setStompClient] = useState(null);
+    const [connected, setConnected] = useState(false);
+    const [greetings, setGreetings] = useState([]);
 
-  function connect() {
-    const socket = new SockJS('http://localhost:8080/gs-guide-websocket');
-    const client = Stomp.over(socket);
-    client.connect({}, frame => {
-      setConnected(true);
-      console.log('Connected: ' + frame);
-      client.subscribe('/topic/greetings', greeting => {
-        showGreeting(JSON.parse(greeting.body).content);
-      });
-      setStompClient(client);
-    });
-  }
-
-  function disconnect() {
-    if (stompClient !== null) {
-      stompClient.disconnect();
+    function connect() {
+        const socket = new SockJS("http://localhost:8080/stomp-end-websocket");
+        const client = Stomp.over(socket);
+        client.connect({}, (frame) => {
+            setConnected(true);
+            console.log("Connected: " + frame);
+            client.subscribe("/topic/backmessage", (greeting) => {
+                showGreeting(JSON.parse(greeting.body).content);
+            });
+            setStompClient(client);
+        });
     }
-    setConnected(false);
-    console.log("Disconnected");
-  }
 
-  function sendName(name) {
-    stompClient.send("/app/hello", {}, JSON.stringify({ name }));
-  }
+    function disconnect() {
+        if (stompClient !== null) {
+            stompClient.disconnect();
+        }
+        setConnected(false);
+        console.log("Disconnected");
+    }
 
-  function showGreeting(message) {
-    setGreetings([...greetings, message]);
-  }
+    function sendName(name) {
+        stompClient.send("/app/frontmessage", {}, JSON.stringify({ name }));
+    }
 
-  return (
-    <div>
-      {!connected && <button id="connect" onClick={connect}>Connect</button>}
-      {connected && <button id="disconnect" onClick={disconnect}>Disconnect</button>}
-      {connected && (
-        <div id="conversation">
-          <input id="name" type="text" />
-          <button id="send" onClick={() => sendName(document.getElementById('name').value)}>Send</button>
-          <table id="greetings">
-            <tbody>
-              {greetings.map((greeting, index) => (
-                <tr key={index}>
-                  <td>{greeting}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+    function showGreeting(message) {
+        setGreetings([...greetings, message]);
+    }
+
+    return (
+        <div>
+            {!connected && (
+                <button id="connect" onClick={connect}>
+                    Connect
+                </button>
+            )}
+            {connected && (
+                <button id="disconnect" onClick={disconnect}>
+                    Disconnect
+                </button>
+            )}
+            {connected && (
+                <div id="conversation">
+                    <input id="name" type="text" />
+                    <button
+                        id="send"
+                        onClick={() =>
+                            sendName(document.getElementById("name").value)
+                        }
+                    >
+                        Send
+                    </button>
+                    <table id="backmessage">
+                        <tbody>
+                            {greetings.map((greeting, index) => (
+                                <tr key={index}>
+                                    <td>{greeting}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default App;
